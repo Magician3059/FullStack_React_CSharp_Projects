10140a300c9363399dbacd63ec790a45401564cf
D1_89388_Rushikesh <rushikeshpatil23052002@gmail.com>
2025-10-07 19:38:43 +0530

    Clean project: remove .vs, bin, obj and other unnecessary files

---
+++ EmployeeAdminPortal/Controllers/EmployeeController.cs
@@ -0,0 +1,95 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.DTO;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        // Add DI of Database
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        //-------------------------------------------------
+
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto newEmp)
+        {
+            // convert DTO into emp object
+            var emp = new Employee()
+            { 
+
+                Email = newEmp.Email,
+                Name = newEmp.Name,
+                Salary = newEmp.Salary,
+                Phone = newEmp.Phone
+            };
+
+            // add emp to employee list
+            dbContext.Employees.Add(emp);
+            // save to database 
+            dbContext.SaveChanges();
+
+            return Ok(emp);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+
+            // get emps from database
+            var emps = dbContext.Employees.ToList();
+
+            if (emps == null || emps.Count == 0) return NotFound();
+            // send all 
+            return Ok(emps);
+        }
+
+
+
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+
+            var emp = dbContext.Employees.Find(id);
+
+            if (emp == null) { return NotFound(); }
+
+            return Ok(emp);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id) {
+
+            var emp = dbContext.Employees.Find(id);
+
+            if (emp == null) { return NotFound(); }
+
+            dbContext.Employees.Remove(emp);
+
+            dbContext.SaveChanges();
+
+            return Ok(emp);
+        } 
+
+
+        
+
+    
+    }
+}
---
+++ EmployeeAdminPortal/DTO/AddEmployeeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAdminPortal.DTO
+{
+    public class AddEmployeeDto
+    {
+        public required String Name { get; set; }
+        public required String Email { get; set; }
+
+        public String? Phone { get; set; }  // ? phone number is nullable props. can be null
+
+        public required decimal Salary { get; set; }
+    }
+}
