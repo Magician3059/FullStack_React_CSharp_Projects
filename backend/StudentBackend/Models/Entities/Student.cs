f7040fbae29a05ade2386c6f181aa9f3d7c8019a
D1_89388_Rushikesh <rushikeshpatil23052002@gmail.com>
2025-10-08 11:25:32 +0530

    Initial commit: add backend and frontend

---
+++ backend/StudentBackend/Models/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentBackend.Models.Entities
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]  // Or use [Precision(18,2)] in EF Core 7+
+        public required decimal Marks { get; set; }
+
+        public String? Report {  get; set; }
+    }
+}
---
+++ backend/StudentBackend/Program.cs
@@ -0,0 +1,63 @@
+
+using Microsoft.EntityFrameworkCore;
+using StudentBackend.Data;
+
+namespace StudentBackend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            // Add CORS policy for all frontends
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", policy =>
+                {
+                    policy.AllowAnyOrigin()      // allow requests from any frontend
+                          .AllowAnyMethod()      // allow GET, POST, PATCH, DELETE, etc.
+                          .AllowAnyHeader();     // allow any headers
+                });
+            });
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+
+            // Register your DbContext with SQL Server
+            builder.Services.AddDbContext<AppDbContext>(options =>
+              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            // use corce policy
+            app.UseCors("AllowAllOrigins");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
